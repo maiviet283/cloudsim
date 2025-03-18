9587df34bf38dd6191e1e5109b1384934d280a35
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-03-18 19:56:23 +0700

    first commit

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/MyCloudSimTest.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/MyCloudSimTest.java
@@ -1,4 +1,16 @@
 package org.cloudbus.cloudsim.examples;
 
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import java.util.Calendar;
+
 public class MyCloudSimTest {
+    public static void main(String[] args) {
+        // Khởi tạo CloudSim
+        int numUsers = 1;
+        Calendar calendar = Calendar.getInstance();
+        CloudSim.init(numUsers, calendar, false);
+
+        System.out.println("CloudSim đã khởi tạo thành công!");
+    }
 }
